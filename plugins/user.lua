0c127232307f3b8f7462fd34e9c532f8077fb2e8
Fwxzxh <jdemeca01@gmail.com>
2023-06-28 01:02:23 -0600

    coloscheme configs

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,43 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      config = function()
+        require("catppuccin").setup {
+          flavor = "Mocha",
+          styles = { 
+            functions = { "bold" }
+          },
+          color_overrides = { 
+          },
+          integrations = {
+            treesitter = true,
+            semantic_tokens = true,
+          },
+          custom_highlights = function(colors)
+            return {
+              ["@variable"] = { fg = colors.flamingo },
+              ["@module"] = { fg = "#F28773" },
+            }
+          end,
+      }
+      end,
+    },
+    {
+      "navarasu/onedark.nvim",
+      name = "onedark",
+      config = function()
+        require("onedark").setup {
+        style = "darker",
+        colros = { 
+          salmon = "#F28773",
+        },
+        highlights = { 
+          ["@variable"] = { fg = "#F28790" }
+        },
+      }
+      end,
+    },
 }
